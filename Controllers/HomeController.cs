c86ea6d05a7fbbe1e0bb201e55e102f2773b0118
marcusruas <marcus.ruas@hotmail.com>
2018-11-19 22:20:33 -0200

    Foi alterado a View About e adicionado diferentes ViewDatas, também aprendi os diferentes redirecionamentos do c#

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC - Curso c#.";
+            ViewData["Aluno"] = "Marcus Ruas";
 
             return View();
         }
