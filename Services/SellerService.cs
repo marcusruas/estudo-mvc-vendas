ded5fd9062a60a53ffa45be17fc75945969829fa
marcusruas <marcus.ruas@hotmail.com>
2018-11-25 16:59:25 -0200

    Criado opção de Edição

--- Services/SellerService.cs
+++ Services/SellerService.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Models;
+using SalesWebMvc.Services.Exceptions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -37,6 +38,23 @@ namespace SalesWebMvc.Services
             var seller = _context.Seller.Find(id);
             _context.Seller.Remove(seller);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller seller)
+        {
+            if(!_context.Seller.Any(x => x.Id == seller.Id))
+            {
+                throw new NotFoundException("Id not Found");
+            }
+            try
+            {
+                _context.Update(seller);
+                _context.SaveChanges();
+            }
+            catch(DbConCurrencyException e)
+            {
+                throw new DbConCurrencyException(e.Message);
+            }
         }
     }
 }
